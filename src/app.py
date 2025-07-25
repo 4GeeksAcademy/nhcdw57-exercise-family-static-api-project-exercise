fd405493d410115b95bcde15d5511c00ca26b985
Nhcdw57 <chocolatedounut@gmail.com>
2025-07-25 19:46:43 +0000

    first commit, all built in tests pass

--- src/app.py
+++ src/app.py
@@ -30,14 +30,69 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_whole_family():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
+    response_body = members
     return jsonify(response_body), 200
 
 
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member_route(member_id):
+    # This is how you can use the Family datastructure by calling its methods
+    member = jackson_family.get_member(member_id)
+    if member == None:
+        return f"Error: Could not find a family member with the id of: {member_id}", 400
+    response_body = {"id": member["id"],
+                     "first_name": member["first_name"],
+                     "age": member["age"],
+                     "lucky_numbers": member["lucky_numbers"]}
+    return jsonify(response_body), 200
+
+@app.route('/members', methods=['POST'])
+def post_member_route():
+    # This is how you can use the Family datastructure by calling its methods
+    response_body = request.json
+    if (not "lucky_numbers" in response_body) or (not "age" in response_body) or (not "first_name" in response_body):
+        return f"Error: invalid request body", 400
+    new_member = {
+        "id":jackson_family._generate_id(),
+        "first_name":response_body["first_name"],
+        "last_name":jackson_family.last_name,
+        "age":response_body["age"],
+        "lucky_numbers":response_body["lucky_numbers"]
+    }
+    member = jackson_family.add_member(new_member)
+    return jsonify(member), 200
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member_route(member_id):
+    # This is how you can use the Family datastructure by calling its methods
+    member = jackson_family.delete_member(member_id)
+    if member == None:
+        return f"Error: Could not find a family member with the id of: {member_id}", 400
+    response_body = {"done":True}
+    return jsonify(response_body), 200
+
+#added a put method cuz realistically, you'd probably want to be able to change family member information
+@app.route('/members/<int:member_id>', methods=['PUT'])
+def put_member_route(member_id):
+    # This is how you can use the Family datastructure by calling its methods
+    member = jackson_family.delete_member(member_id)
+    if member == None:
+        return f"Error: Could not find a family member with the id of: {member_id}", 400
+    response_body = request.json
+    if (not "lucky_numbers" in response_body) or (not "age" in response_body) or (not "first_name" in response_body):
+        return f"Error: invalid request body", 400
+    updated_member = {
+        "id":member_id,
+        "first_name":response_body["first_name"],
+        "last_name":jackson_family.last_name,
+        "age":response_body["age"],
+        "lucky_numbers":response_body["lucky_numbers"]
+    }
+    member = jackson_family.add_member(updated_member)
+    return jsonify(member), 200
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
